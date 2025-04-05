10c7f0b447570d9db87b07345567dc3e4c823572
hotroso <hotrogiaiphapso@gmail.com>
2025-04-05 22:38:21 +0700

    update git ignore

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
+require('dotenv').config({path: '.env'})
 const express = require("express");
-var app = express();
+const app = express();
 const path = require("path");
-var cors = require("cors"); // xử lý dư liệu dạng json
+const cors = require("cors"); // xử lý dư liệu dạng json
 const { dbconnect } = require("./app/configs/dbConfig");
 
 app.use(express.json()); // để xử lý dạng dữ liệu json
 app.use(express.urlencoded({extended:true})); // để xử lý dữ liệu url encoded
 app.use(cors({credentials:true,origin:"*"})); // chấp thuận cors từ mọi u
 
+const SERVER_PORT = process.env.SERVER_PORT ?? 3002
 
-dbconnect();
-var userRouter= require('./app/routes/UserRoute')
+dbconnect()
+    .then(r => console.log("connect to db success"));
+const userRouter = require('./app/routes/UserRoute');
 app.use("/api/user", userRouter);
 
-var productRouter = require('./app/routes/ProductRoute')
+const productRouter = require('./app/routes/ProductRoute');
 app.use("/api/product", productRouter);
 
 // var categoryRouter = require('./app/routes/CategoryRoute')
@@ -23,6 +26,6 @@ app.get('/',(req,res,next)=>{
     res.send("Vui ve");
 })
 
-app.listen(3002, () => {
-    console.log(`Server srtarted on port`);
+app.listen(SERVER_PORT, () => {
+    console.log(`Server started on port ${SERVER_PORT}`);
 });
